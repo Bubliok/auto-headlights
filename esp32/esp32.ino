02417d8680d93008683134fb17667e9677db1d50
bubliok <bubliok@outlook.com>
2025-03-01 01:38:26 +0200

    light control implementation

--- esp32/esp32.ino
+++ esp32/esp32.ino
@@ -6,11 +6,11 @@
 #include <map>
 
 // Defaults
-#define RELAY_PIN 2
+#define HEADLIGHT_PIN 12
 #define LDR_PIN 34
 #define ACC_PIN 33
 #define PARKING_PIN 32
-#define BLINKER_PIN 31
+#define UNLOCK_PIN 35
 
 std::map<String, int> settings = 
     {
@@ -18,7 +18,7 @@ std::map<String, int> settings =
         {"off_threshold", 500},
         {"sun_threshold", 600},
         {"hysteresis", 50},
-        {"sample_size", 10},
+        {"sample_count", 10},
         {"read_delay", 10},
         {"night_mode", 300000},
         {"night_mode_off", 1800000},
@@ -30,6 +30,11 @@ std::map<String, int> defaults = settings;
 
 const char* ssid = "ALS01";
 const char* password = "123456789";
+
+bool lightsOn = false;
+unsigned long darkStartTime = 0;
+unsigned long brightStartTime = 0;
+bool isNightMode = false;
 
 AsyncWebServer server(80);
 Preferences prefs;
@@ -47,8 +52,12 @@ void setup() {
     loadPreferences();
 
     pinMode(LDR_PIN, INPUT);
-    pinMode(RELAY_PIN, OUTPUT);
-    digitalWrite(RELAY_PIN, HIGH);
+    pinMode(UNLOCK_PIN, INPUT);
+    pinMode(ACC_PIN, INPUT);
+    pinMode(HEADLIGHT_PIN, OUTPUT);
+    pinMode(PARKING_PIN, OUTPUT);
+
+    digitalWrite(HEADLIGHT_PIN, HIGH);
 
     WiFi.softAP(ssid, password);
     Serial.print("AP IP address: ");
@@ -75,7 +84,94 @@ void setup() {
     Serial.println("Server Started");
 }
 
-void loop() {}
+void loop() {
+    int lightLevel = readLDR();
+    bool shouldBeOn = checkLightCondition(lightLevel);
+    updateLights(shouldBeOn);
+    // Serial.println(lightLevel);
+    // Serial.println(analogRead(LDR_PIN));
+    // Serial.println(settings["on_threshold"]);
+    debug(lightLevel);
+    delay(100);
+}
+
+int readLDR() { // reads the light sensor value and returns the average of the last n samples
+    static std::vector<int> samples(settings["sample_count"], 0);
+    static int i = 0;
+    static long sum = 0;
+
+    if (samples.size() != settings["sample_count"]) {
+        samples.assign(settings["sample_count"], 0);
+        sum = 0;
+        i = 0;
+    }
+
+    sum -= samples[i];
+    samples[i] = analogRead(LDR_PIN);
+    sum += samples[i];
+    i = (i + 1) % settings["sample_count"];
+
+    delay(settings["read_delay"]);
+
+    return sum / settings["sample_count"];
+}
+
+bool checkLightCondition(int lightLevel){
+    unsigned long currentTime = millis();
+
+    if (lightLevel < settings["on_threshold"]) { //enters night mode if light level is below the on threshold for n seconds
+        brightStartTime = 0;
+        if (darkStartTime == 0) darkStartTime = currentTime;
+
+        if (!isNightMode && (currentTime - darkStartTime >= settings["night_mode"])) {
+            isNightMode = true;
+            Serial.println("Night Mode Activated");
+        }
+        return true;
+    } else {
+        darkStartTime = 0;
+       
+        if (lightLevel >= settings["sun_threshold"]) { //exit night mode if sunlight is detected
+            if (isNightMode) {
+                isNightMode = false;
+                Serial.println("Sunlight Detected - Exiting Night Mode");
+            }
+            return false;
+        }
+
+        if (lightLevel > settings["off_threshold"]) { //exits night mode is the light level is above the off threshold for n seconds
+            if (brightStartTime == 0) brightStartTime = currentTime;
+            
+            if (isNightMode && (currentTime - brightStartTime >= settings["night_mode_off"])) {
+                isNightMode = false;
+                Serial.println("Exiting Night Mode...");
+            }
+        } else {
+            brightStartTime = 0;
+        }
+        return !isNightMode ? false : lightsOn;
+    }
+}
+
+void updateLights(bool shouldBeOn) {
+    if (shouldBeOn != lightsOn) {
+        digitalWrite(HEADLIGHT_PIN, shouldBeOn ? HIGH : LOW);
+        lightsOn = shouldBeOn;
+        Serial.println(lightsOn ? "Lights On" : "Lights Off");
+    }
+}
+
+void debug(int lightLevel) {
+    Serial.print("Light Level: ");
+    Serial.println(lightLevel);
+    Serial.print("Night Mode: ");
+    Serial.println(isNightMode ? "Yes" : "No");
+    Serial.print("Lights are: ");
+    Serial.println(lightsOn ? "Yes" : "No");
+    Serial.println();
+}
+
+// -------------------------------------------------------------
 
 void savePreferences(AsyncWebServerRequest *request) {
     for (auto &pair : settings) {
